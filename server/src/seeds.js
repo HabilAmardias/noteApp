4215ac57a89b5ead224e7c136fe053826c03ab58
Muhammad Habil Amardias <habilamar@gmail.com>
2023-01-05 15:08:56 +0700

    moving files

--- server/src/seeds.js
+++ server/src/seeds.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const Note = require('./src/models/Note.js')
+const Note = require('./models/Note.js')
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
