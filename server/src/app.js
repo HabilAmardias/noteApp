5adf87b3fa3c97385bf60849f1fe3a52a8332813
Muhammad Habil Amardias <habilamar@gmail.com>
2023-01-05 15:11:56 +0700

    app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Note = require('./src/models/Note.js')
-const ErrorHandling = require('./src/ErrorHandling')
+const Note = require('./models/Note.js')
+const ErrorHandling = require('./ErrorHandling')
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
